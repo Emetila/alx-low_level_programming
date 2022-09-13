eaaf9bad5e75a2e815f7689eb7f7f5318e401765
root <you@example.com>
2022-09-12 17:15:51 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stfio.h>
+
+/**
+* Main - print the string in the put function
+* 
+* Descriptrion: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
